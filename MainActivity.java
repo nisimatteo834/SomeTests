70da3e0ff7dbc53fd18b7862a934be471f1d0921
Matteo <nisimatteo834@gmail.com>
2017-11-26 15:32:04 +0100

    SomeTest

--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.sometests;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.text.format.Formatter;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,94 +16,80 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.net.wifi.WifiManager;
+import com.example.android.sometests.Wifi;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends Activity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
+        this.updateValues();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    protected  void onResume(){
+        super.onResume();
+        this.updateValues();
+
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
+    private void updateValues(){
+        final WifiManager wifi = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+
+        if (!wifi.isWifiEnabled()) {
+
+            if (wifi.getWifiState() != WifiManager.WIFI_STATE_ENABLING) {
+
+                wifi.setWifiEnabled(true);
             }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        Menu choices = navigationView.getMenu();
-        MenuItem nav_camera = choices.findItem(R.id.nav_camera);
-        nav_camera.setTitle("Maybe I changed the title");
-        navigationView.setNavigationItemSelectedListener(this);
-
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the SideMenu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        TextView ssid_value = (TextView) findViewById(R.id.SSID_value);
+        ssid_value.setText(wifi.getConnectionInfo().getSSID());
+
+        TextView ip_value = (TextView) findViewById(R.id.ip_value);
+        ip_value.setText(Formatter.formatIpAddress(wifi.getConnectionInfo().getIpAddress()));
+
+        TextView speed_value = (TextView) findViewById(R.id.speed_value);
+        String s = String.valueOf(wifi.getConnectionInfo().getLinkSpeed()) + " Mbit/s";
+        speed_value.setText(s);
+
+
+        TextView mac_value = (TextView) findViewById(R.id.MAC_value);
+        mac_value.setText(Wifi.getMacAddr());
+
+        TextView rssi = (TextView) findViewById(R.id.rssi_value);
+        rssi.setText(String.valueOf(wifi.getConnectionInfo().getRssi()));
+
+        TextView GW = (TextView) findViewById(R.id.gateway_value);
+        GW.setText(Formatter.formatIpAddress(wifi.getDhcpInfo().gateway));
+
+
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
---
+++ Wifi.java
@@ -0,0 +1,47 @@
+package com.example.android.sometests;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.SensorManager;
+import android.hardware.Sensor;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+import android.widget.TextView;
+import java.lang.String;
+
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+public class Wifi {
+
+    public static String getMacAddr() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(Integer.toHexString(b & 0xFF) + ":");
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+            //handle exception
+        }
+        return "";
+    }
+}
